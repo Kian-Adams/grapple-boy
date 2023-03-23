0beb8d7d3211697870d5304b122aeef988b618cd
DB771 <77589293+DB771@users.noreply.github.com>
2023-03-23 11:18:02 -0400

    WorkingDemo1.1
    • Added Running
    • Added different Jump heights

--- WorkingDemo1.c
+++ WorkingDemo1.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/bios.h>
 #include <sys/libwwc.h>
 #include "Ground.h"
@@ -28,6 +30,15 @@ void init_sprite()
     } else {
 	sprite_set_char(0, 0);
 	sprite_set_char(1, 1);
+    }
+}
+void clear_screen(BYTE screen, BOOL stripe)
+{
+    int i;
+
+    for (i = 0; i < 32; i++) {
+	screen_fill_char(screen, i, 0, 1, 32,
+	    0 + (stripe ? (1 - i << CFSFT_PALETTE) : 0));
     }
 }
 void subPlatformSet(int iScr ,int iX ,int iY ,int iWmax ,int iHmax ,int iPal,int iChn)
@@ -61,7 +72,7 @@ void groundinit(){
     subTextureSet(SCREEN2 ,0 ,10,SCREEN_CHAR_WIDTH, 8                  ,0,2);
 }
 void main(int argc, char *argv[]) {
-	int i, iJmp, iChn;
+	int i, j, k, iJmp, iChn;
 	BYTE x, y;
 	WORD key_data;
 	init_sprite();
@@ -75,10 +86,13 @@ void main(int argc, char *argv[]) {
 	palette_set_color(13, 0x7520);
 
 	/* screen setting */
-	font_set_colordata(5, Ground_width * Ground_height, bmp_clear);
-	screen_fill_char(SCREEN1, 0, 0, 32, 32, 0);
-	screen_set_scroll(SCREEN1, 0, 0);
+	clear_screen(SCREEN1, TRUE);
 	groundinit();
+	font_set_colordata(0, Ground_width * Ground_height, bmp_clear);
+	font_set_colordata(2, Ground_width * Ground_height, bmp_Ground);
+	screen_fill_char(SCREEN2, 0, 0, 32, 32, 0);
+	screen_set_scroll(SCREEN1, 0, 0);
+
 
 	/* sprite setting */
 	font_set_colordata(4, 16, bmp_Grapple_Boy4color);
@@ -110,45 +124,70 @@ void main(int argc, char *argv[]) {
 	iJmp=0;	
 	iChn=0;
 	i=0;
+	j=0;
+	k=0;
 	do {
 		BYTE x2, y2;
 
 		sys_wait(1);
 		i++;
-		if (i == 100){
+		if (i == 3){
 			i = 0;
 		}
 		key_data = key_press_check();
 		if(iJmp==JMP_ED)
 		{	
 			iChn=screen_get_char1(SCREEN1 , x/8 , (y+16)/8) & CFM_FONT;
-			if(iChn==0)
+			if((iChn==0)&(k==0))
 			{
 				y=y+2;
 			}else{
 			    if (key_data & KEY_A){
-					iJmp=JMP_ST;
+					if (k<8){
+						j=2;
+						j = j+(j/2);
+						k++;
+						iJmp = j;
+					}else{
+						k=0;
+					}
+					if (j > JMP_ST){
+						j = JMP_ST;
+					}
+				}else{
+					j=0;
+					k=0;
 				}
 			}
 		}else{		
 			iJmp--;		
-			y=y-2;  
+			if (y>0){
+			y=y-2;
+			}  
 		}
 		if (key_data & KEY_LEFT1){
 			if ((i%10)==0){
-				font_set_colordata(4, 16, bmp_Grapple_Boy4colorWalkL);
-			}else{
 				font_set_colordata(4, 16, bmp_Grapple_Boy4colorL);
+			}else{
+				font_set_colordata(4, 16, bmp_Grapple_Boy4colorWalkL);
 			}
-			x--;
+			if (key_data & KEY_B){
+				x = x - 3;
+			}else{
+				x--;
+			}
 		}
 		if (key_data & KEY_RIGHT1){
 			if ((i%10)==0){
-				font_set_colordata(4, 16, bmp_Grapple_Boy4colorWalk);
-			}else{
 				font_set_colordata(4, 16, bmp_Grapple_Boy4color);
+			}else{
+				font_set_colordata(4, 16, bmp_Grapple_Boy4colorWalk);
 			}
-			x++;
+			if (key_data & KEY_B){
+				x = x + 3;
+			}else{
+				x++;
+			}
 		}
 		x2 = x + 8;
 		y2 = y + 8;
