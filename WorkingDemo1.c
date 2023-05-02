fbdbc5a696695170db7c41697f0ca4db0751d7cd
Darren Bonifacio <77589293+DB771@users.noreply.github.com>
2023-05-02 12:18:40 -0400

    Working Demo 5/2/23
    • Added time slow ability to be changed later (activate with down arrow/X3)
    • Added Sword Slash animation cycle
    • WIP- check for X D-Pad button press

--- WorkingDemo1.c
+++ WorkingDemo1.c
@@ -8,6 +8,14 @@
 #include "Grapple_Boy4color-L.h"
 #include "Grapple_Boy4color-WalkL.h"
 #include "LanceGuardF1_3.h"
+#include "GBuplunge.h"
+#include "GBrightlunge.h"
+#include "GBdownlunge.h"
+#include "GBleftlunge.h"
+#include "GBslash1.h"
+#include "GBslash2.h"
+#include "GBslash3.h"
+
 void init_sprite()
 {
     if (wwc_get_hardarch() == HARDARCH_WSC) {
@@ -139,15 +147,8 @@ int pause()
     }
 }
 
-
-/* void pause(){
-	
-}
-
-*/
-
 void main(int argc, char *argv[]) {
-	int i, sprite_cycle, direction, jump_height_potential, y_press, l_momentum, r_momentum, slash_anim, lunge, lunge_lag, lunge_anim, guard_anim, iJmp, iChn;
+	int i, sprite_cycle, direction, jump_height_potential, x_press, y_press , l_momentum, r_momentum, slash_anim, lunge, lunge_lag, lunge_anim, guard_anim, iJmp, iChn;
 	BYTE x, y;
 	BYTE loop;
 	WORD key_data;
@@ -174,7 +175,7 @@ void main(int argc, char *argv[]) {
 	font_set_colordata(4, 16, bmp_Grapple_Boy4color);
 	for(i = 0; i < 128; i++)
 		sprite_set_char_location(i, 0, 0xf8, 0xf8);
-	sprite_set_range(0, 128);
+	sprite_set_range(0, 128);	
 
 	/* display on */
 	display_control(DCM_SCR1 | DCM_SPR);
@@ -203,6 +204,7 @@ void main(int argc, char *argv[]) {
 	iChn = 0; /* If iChn == 0, then player is in Freefall. */
 	i = 0; /* Variable for incrementing the sprite cycle. */
 	jump_height_potential = 0; /* Jump height potential. The longer A is held, the more k is incremented. Max Jump height is 8 units. */
+	x_press = 0; /* Tracks whether the player is pressing the X button. 0 = No and 1 = Yes */
 	y_press = 0; /* Tracks whether the player is pressing the Y button. 0 = No and 1 = Yes */
 	l_momentum = 0; /* Left momentum */
 	r_momentum = 0; /* Right momentum */
@@ -261,6 +263,10 @@ void main(int argc, char *argv[]) {
 			if (y>0){
 				y=y-2;
 			}
+		}
+		/* Check whether an X D-Pad button is pressed. */
+		if (KEY_UP1 || KEY_RIGHT1 || KEY_DOWN1 || KEY_LEFT1){
+			x_press=1;
 		}
 		/* Check whether a Y D-Pad button is pressed. */
 		if (KEY_Y1 || KEY_Y2 || KEY_Y3 || KEY_Y4){
@@ -345,6 +351,7 @@ void main(int argc, char *argv[]) {
 				}
 			}
 		}
+		
 		/* High-Frequency Sword attacks */
 		if (key_data & KEY_B){
 			if ((lunge == 1) & (y_press == 1) & (lunge_lag == 0)) { 
@@ -352,6 +359,7 @@ void main(int argc, char *argv[]) {
 				lunge_lag=50;
 				if (key_data & KEY_Y1){ /* If player presses Y1 (Up), they lunge in said direction. */
 					y = y - 32;
+					font_set_colordata(4, 16, bmp_GBuplunge);
 				}
 				else if (key_data & KEY_Y2){ /* If player presses Y2 (Right), they lunge in said direction. */
 					direction = 1;
@@ -359,10 +367,11 @@ void main(int argc, char *argv[]) {
 					if (iChn==0){ /* If player is in Freefall, they resist gravity enough to move laterally straight */
 						y = y - 2;
 					}
-					font_set_colordata(4, 16, bmp_Grapple_Boy4color);
+					font_set_colordata(4, 16, bmp_GBrightlunge);
 				}
 				else if (key_data & KEY_Y3){ /* If player presses Y3 (Down), they lunge in said direction. */
 					y = y + 30;
+					font_set_colordata(4, 16, bmp_GBdownlunge);
 				}
 				else if (key_data & KEY_Y4){ /* If player presses Y4 (Left), they lunge in said direction. */
 					direction = 0;
@@ -370,29 +379,27 @@ void main(int argc, char *argv[]) {
 					if (iChn==0){ /* If player is in Freefall, they resist gravity enough to move laterally straight */
 						y = y - 2;
 					}
-					font_set_colordata(4, 16, bmp_Grapple_Boy4colorL);
+					font_set_colordata(4, 16, bmp_GBleftlunge);
 				}
-			} /*
-		    else if (slash_anim < 4){
-				slash_anim++;
-				font_set_colordata(6, 72, bmp_LanceGuardF1_3);
 			}
-			else if (slash_anim < 8){
-				slash_anim++;
-				font_set_colordata(6, 72, bmp_LanceGuardF1_3);
+			else/* if (x_press == 0) */{
+		    	if (slash_anim < 4){
+					slash_anim++;
+					font_set_colordata(6,72, bmp_GBslash1);
+				}
+				else if (slash_anim < 8){
+					slash_anim++;
+					font_set_colordata(6,72, bmp_GBslash2);
+				}
+				else if (slash_anim < 12){
+					slash_anim++;
+					font_set_colordata(6,72, bmp_GBslash3);
+				}
+				else {
+					slash_anim = 0;
+					font_set_colordata(4,16, bmp_Grapple_Boy4colorWalk);
+				}
 			}
-			else if (slash_anim < 12){
-				slash_anim++;
-				font_set_colordata(6, 72, bmp_LanceGuardF1_3);
-			}
-			else if (slash_anim < 16){
-				slash_anim++;
-				font_set_colordata(6, 72, bmp_LanceGuardF1_3);
-			}
-			else {
-				slash_anim = 0;
-				font_set_colordata(4, 16, bmp_Grapple_Boy4colorWalk);
-			} */
 		}
 
 		/* Charging Lance attacks */
@@ -415,6 +422,7 @@ void main(int argc, char *argv[]) {
 		
 		*/
 
+		x_press = 0; /* Resets the check for an X button press. */
 		y_press = 0; /* Resets the check for a Y button press. */
 		x2 = x + 8;
 		y2 = y + 8;
@@ -423,9 +431,16 @@ void main(int argc, char *argv[]) {
 		sprite_set_location(0, x, y);
 		sprite_set_location(1, x2, y);
 		sprite_set_location(2, x, y2);
-		sprite_set_location(3, x2, y2); /*
+		sprite_set_location(3, x2, y2);
+		/* call these only when needed */
 		sprite_set_location(4, x3, y);
-		sprite_set_location(6, x3, y2); */
+		sprite_set_location(5, x, y3);
+		sprite_set_location(6, x3, y2);
+		sprite_set_location(7, x2, y3);
+		sprite_set_location(8, x3, y3);
+		if (key_data & KEY_DOWN1){
+			sys_wait(1);
+		}
 		if (key_data & KEY_START) {
 	    	loop = pause();
 			if (loop == 0) {
